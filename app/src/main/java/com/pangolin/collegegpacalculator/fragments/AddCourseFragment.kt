17a0b8e35c3d5cbc5afdbb410b0074368734b38f
Charaf <charaf4charaf@gmail.com>
2021-09-23 14:48:48 +0300

    gradle update

--- app/src/main/java/com/pangolin/collegegpacalculator/fragments/AddCourseFragment.kt
+++ app/src/main/java/com/pangolin/collegegpacalculator/fragments/AddCourseFragment.kt
@@ -1,7 +1,8 @@
 package com.pangolin.collegegpacalculator.fragments
 
+import android.app.Fragment
 import androidx.fragment.app.Fragment
 
 class AddCourseFragment : Fragment() {
-    override on
+
 }
