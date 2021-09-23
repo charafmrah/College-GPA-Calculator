201598075cfafb8c27bdf1ba63998ee2ed90b7b0
Charaf <charaf4charaf@gmail.com>
2021-09-23 13:49:47 +0300

    renamed files

---
+++ app/src/main/java/com/pangolin/collegegpacalculator/fragments/AddCourseFragment.kt
@@ -0,0 +1,7 @@
+package com.pangolin.collegegpacalculator.fragments
+
+import androidx.fragment.app.Fragment
+
+class AddCourseFragment : Fragment() {
+    override on
+}
--- app/src/main/java/com/pangolin/collegegpacalculator/fragments/CourseAddFragment.kt
+++
@@ -1,4 +0,0 @@
-package com.pangolin.collegegpacalculator.fragments
-
-class CourseAddFragment {
-}
