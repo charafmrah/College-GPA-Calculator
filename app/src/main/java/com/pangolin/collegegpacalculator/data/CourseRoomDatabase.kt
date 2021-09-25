b57540b1920ab19581a5acdfbaa016b09ee707cd
Charaf <charaf4charaf@gmail.com>
2021-09-25 18:00:47 +0300

    implemented CourseRoomDatabase

---
+++ app/src/main/java/com/pangolin/collegegpacalculator/data/CourseRoomDatabase.kt
@@ -0,0 +1,32 @@
+package com.pangolin.collegegpacalculator.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Course::class], version = 1, exportSchema = false)
+abstract class CourseRoomDatabase : RoomDatabase() {
+
+    abstract fun courseDao(): CourseDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: CourseRoomDatabase? = null
+
+        fun getDatabase(context: Context): CourseRoomDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    CourseRoomDatabase::class.java,
+                    "course_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
