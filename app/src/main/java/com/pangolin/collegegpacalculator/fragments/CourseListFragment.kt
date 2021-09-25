8c7a1778182aab7c39a7d22c81c39c603bc9ce23
Charaf <charaf4charaf@gmail.com>
2021-09-25 16:59:21 +0300

    implemented CourseListFragment

--- app/src/main/java/com/pangolin/collegegpacalculator/fragments/CourseListFragment.kt
+++ app/src/main/java/com/pangolin/collegegpacalculator/fragments/CourseListFragment.kt
@@ -1,6 +1,38 @@
 package com.pangolin.collegegpacalculator.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.pangolin.collegegpacalculator.R
+import com.pangolin.collegegpacalculator.databinding.FragmentCourseListBinding
 
 class CourseListFragment : Fragment() {
+
+    private var _binding: FragmentCourseListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCourseListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.floatingActionButton.setOnClickListener {
+            val action = CourseListFragmentDirections.actionCourseListFragmentToAddCourseFragment(
+                getString(R.string.add_fragment_title)
+            )
+            this.findNavController().navigate(action)
+        }
+    }
+
 }
