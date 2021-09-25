329535500e0a4c12db540cd9078056fd8e8efc31
Charaf <charaf4charaf@gmail.com>
2021-09-25 18:14:11 +0300

    added a file to hold final values

---
+++ app/src/main/java/com/pangolin/collegegpacalculator/ListOfGradesAndCredits.kt
@@ -0,0 +1,9 @@
+package com.pangolin.collegegpacalculator
+
+val gradeLetters: List<String> = listOf(
+    "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "D-", "F",
+)
+
+val credits: List<Int> = listOf(
+    0, 1, 2, 3, 4
+)
