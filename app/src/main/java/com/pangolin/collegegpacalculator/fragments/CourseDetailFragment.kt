9a1369b149714407e9a09e7f865c597bb77ace37
Charaf <charaf4charaf@gmail.com>
2021-09-25 17:38:04 +0300

    inflated course detail and added room dependencies

--- app/src/main/java/com/pangolin/collegegpacalculator/fragments/CourseDetailFragment.kt
+++ app/src/main/java/com/pangolin/collegegpacalculator/fragments/CourseDetailFragment.kt
@@ -10,4 +10,20 @@ import com.pangolin.collegegpacalculator.databinding.FragmentCourseDetailBinding
 
 class CourseDetailFragment: Fragment() {
 
+    private var _binding: FragmentCourseDetailBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCourseDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
