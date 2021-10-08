0f9b0606516b5d99ab250c397d80ff02495c3cb9
Charaf <charaf4charaf@gmail.com>
2021-10-08 15:25:54 +0300

    more comments

--- app/src/main/java/com/pangolin/collegegpacalculator/CalculatorApplication.kt
+++ app/src/main/java/com/pangolin/collegegpacalculator/CalculatorApplication.kt
@@ -19,6 +19,8 @@ import android.app.Application
 import com.pangolin.collegegpacalculator.model.CourseRoomDatabase
 
 class CalculatorApplication : Application() {
+    // Using by lazy so the database is only created when needed
+    // rather than when the application starts
     val database: CourseRoomDatabase by lazy {
         CourseRoomDatabase.getDatabase(this)
     }
--- app/src/main/java/com/pangolin/collegegpacalculator/viewmodels/CalculatorViewModel.kt
+++ app/src/main/java/com/pangolin/collegegpacalculator/viewmodels/CalculatorViewModel.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.pangolin.collegegpacalculator.viewmodels
 
 import androidx.lifecycle.*
@@ -6,8 +21,13 @@ import com.pangolin.collegegpacalculator.model.CourseDao
 import kotlinx.coroutines.launch
 import kotlin.math.roundToLong
 
+/**
+ * View Model to keep a reference to the GPA Calculator repository and an up-to-date list of all courses.
+ *
+ */
 class CalculatorViewModel(private val courseDao: CourseDao) : ViewModel() {
 
+    // Cache all courses from the database using LiveData
     val allCourses: LiveData<List<Course>> = courseDao.getCourses().asLiveData()
 
     fun calculateGpa(): Double {
